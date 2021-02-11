07c44f0dbf1f0becc9629a266b8b5ae06d41839d
Diego Lipinski <diegocastroh20@gmail.com>
2021-02-10 22:39:21 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9bb195c4a301b8dcc0da9595fb07ff02'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '56543911-0c5d-4da5-a8b8-25dda7f5e45a'
+};
