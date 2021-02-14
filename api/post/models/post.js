6a080101d4567f27c28920729ad2e437721d08f4
Diego Lipinski <diegocastroh20@gmail.com>
2021-02-14 13:55:49 -0300

    post lifecycle

--- api/post/models/post.js
+++ api/post/models/post.js
@@ -1,8 +1,30 @@
 'use strict';
 
+const slugify = require('slugify');
+const readingTime = require('reading-time');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        async beforeCreate(data) {
+            if (data.title) {
+                data.slug = slugify(data.title, {lower: true});
+            }
+
+            const stats = readingTime(data.content);
+            data.timing = stats.text;
+        },
+        async beforeUpdate(params, data) {
+            if (data.title) {
+                data.slug = slugify(data.title, {lower: true});
+            }
+
+            const stats = readingTime(data.content);
+            data.timing = stats.text;
+        },
+    },
+};
